c1542b783df73abf1a529e4f6a531a31b5563dbf
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-06-21 14:12:58 -0500

    test pass

--- src/app/cards-api.service.spec.ts
+++ src/app/cards-api.service.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CardsApiService } from './cards-api.service';
+import { HttpClient } from '@angular/common/http';
 
 describe('CardsApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  const httpSpy = jasmine.createSpyObj('HttpClient', ['get']);
+
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      { provide: HttpClient, useValue: httpSpy }
+    ]
+  }));
 
   it('should be created', () => {
     const service: CardsApiService = TestBed.get(CardsApiService);
